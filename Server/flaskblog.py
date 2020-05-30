aa7bb2c4abb93221e07b3f4c861a16b08fb3bb3b
hipros <npclinic3@gmail.com>
2020-05-31 03:26:01 +0900

    사용자 이미지 전처리 (얼굴 부분 잘라서 frame 맞추기, image 그대로 request, response) /crop/<image_name>

--- Server/flaskblog.py
+++ Server/flaskblog.py
@@ -1,11 +1,14 @@
 # -*- encoding: utf8 -*-
+import os
 import base64
+import cv2
 from flask import Flask, render_template
 from flask_restful import Api, request
 from paramiko import SSHClient
-import os
 from FaceSwap import face_main
 from celery import Celery
+from dlib import get_frontal_face_detector as face_detector
+
 
 # celery 동작 명령어
 # celery worker -A flaskblog.celery --loglevel=INFO --pool=solo
@@ -43,7 +46,9 @@ port ="NIPA_GPU_SERVER_PORT"
 password = "NIPA_GPU_SERVER_PASSWORD"
 
 
-root_dir = 'ROOT_DIR'
+#root_dir = ROOT_DIR
+root_dir = '/tmp'
+
 source_img_dir = os.path.join(root_dir, 'source-image')
 target_img_dir = os.path.join(root_dir, 'target-image')
 result_img_dir = os.path.join(root_dir, 'result-image')
@@ -81,6 +86,75 @@ def onlySwap(img_name):
                            sour_name='images/source-image/' + img_name + '.png',
                            tar_name='images/target-image/' + img_name + '.png',
                            ret_name='images/result-image/' + img_name + '.png')
+
+#처음 전송받은 이미지 crop
+@app.route('/crop/<img_name>', methods=['POST'])
+def cropping(img_name):
+    #b64_string = request.form.get('image')
+    #image = base64.b64decode(b64_string)
+    image = request.files['image']
+    image.save(os.path.join(source_img_dir,"test.jpg"))
+    image = cv2.imread(os.path.join("/home/rtos/바탕화면", "test.jpg"), cv2.IMREAD_COLOR)
+    faces = face_detector()(image)
+    image_height, image_width = image.shape[:2]
+
+    if(len(faces) != 1):
+        # 얼굴이 하나가 아닐경우
+        print('얼굴이 하나가 아님')
+        return '0'
+
+    face = faces[0]
+    face_width = face.right()-face.left()
+    face_height = face.bottom()-face.top()
+    face_center = [(face.bottom()+face.top())/2, (face.right()+face.left())/2]
+
+    if(face_width < 250 or face_height < 250):
+        #얼굴이 너무 작을 경우
+        print('얼굴이 작음')
+        print(face_width, face_height)
+        return '0'
+
+    # ffhq image 얼굴 크기
+    ffhq_face_size = 0
+
+    # ffhq image frame [좌, 우, 상, 하 마진 길이]
+    ffhq_image_frame = []
+
+    # 변경할 비율
+    image_change_rate = 0
+
+    # 각 숫자는 ffhq의 data frame 값
+    if (face_width >= 500):
+        ffhq_face_size = 642
+        ffhq_image_frame = [199, 183, 270, 112]
+    else:
+        ffhq_face_size = 535
+        ffhq_image_frame = [225, 264, 344, 145]
+
+    image_rate = face_width/ffhq_face_size
+    result_image_frame = [_*image_rate for _ in ffhq_image_frame]
+
+    # result image frame [좌, 우, 상, 하 마진 길이]
+    result_image_left = int(face_center[1] - face_width / 2 - result_image_frame[0])
+    result_image_right = int(face_center[1] + face_width / 2 - result_image_frame[1])
+    result_image_top = int(face_center[0] - face_height / 2 - result_image_frame[2])
+    result_image_bottom = int(face_center[0] + face_height / 2 - result_image_frame[3])
+
+    if (result_image_left < 0 or result_image_top < 0 or
+            result_image_right > image_width or result_image_bottom > image_height):
+        # 얼굴이 너무 한쪽에 치우쳐 있을 때
+        return '0'
+
+    cv2.imwrite('/home/rtos/바탕화면/before.jpg', image)
+
+    result_image = image[result_image_top:result_image_bottom, result_image_left:result_image_right]
+    cv2.imwrite('/home/rtos/바탕화면/after.jpg', result_image)
+
+    result_image = cv2.resize(result_image, (1024, 1024), interpolation=cv2.INTER_CUBIC)
+    return result_image
+
+    #result_image_base64 = base64.b64encode(result_image)
+    #return result_image_base64
 
 
 @app.route('/start/<img_name>', methods=['POST'])
